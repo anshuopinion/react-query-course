dd58d114dd17947adb463ef39dc0faf26e99da09
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2024-01-16 18:12:22 +0530

    Refactor blog controller to include pagination

--- backend/src/controllers/blog/index.ts
+++ backend/src/controllers/blog/index.ts
@@ -1,21 +1,36 @@
 import { RequestHandler } from "express";
 import Blog from "../../models/blog";
-import createHttpError, {
-  InternalServerError,
-  Conflict,
-  NotFound,
-} from "http-errors";
+import { InternalServerError, Conflict, NotFound } from "http-errors";
 import {
   CreateBlogType,
   DeleteByIdType,
   GetByIdType,
   UpdateBlogType,
 } from "../../validation/blog";
+
 import { validateObjectId } from "../../utils/validateObjectId";
+
 const getAll: RequestHandler = async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (Number(page) - 1) * Number(limit);
+
   try {
-    const blogs = await Blog.find();
-    return res.json(blogs);
+    const blogs = await Blog.find().skip(skip).limit(Number(limit));
+    const totalBlogs = await Blog.countDocuments();
+    if (blogs.length === 0) next(NotFound("No blogs found"));
+
+    const totalPages = Math.ceil(totalBlogs / Number(limit));
+    const isLastPage = Number(page) === totalPages;
+    const isFirstPage = Number(page) === 1;
+
+    return res.json({
+      blogs,
+      pagination: {
+        totalPages,
+        first: isFirstPage,
+        last: isLastPage,
+      },
+    });
   } catch (error) {
     next(InternalServerError());
   }
@@ -70,7 +85,6 @@ const updateById: RequestHandler<{}, any, UpdateBlogType["body"]> = async (
       { title, content },
       { new: true }
     );
-
     res.json(blog);
   } catch (error) {
     next(InternalServerError());
